42b4abd4813092f44db4b61bbaaf3c9a920c8465
Sanjay14-nw <sb0295564@gmail.com>
2025-09-19 14:18:15 +0530

    Add files via upload

---
+++ q34.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int fib(int n);
+
+int main() {
+    printf("%d",fib(6));
+
+    return 0;
+}
+
+int fib(int n) {
+    if(n==0){return 0;}
+    if(n==1){return 1;}
+
+    int fib1=fib(n-1);
+    int fib2=fib(n-2);
+    int fib3=fib1+fib2;
+    return fib3;
+}
